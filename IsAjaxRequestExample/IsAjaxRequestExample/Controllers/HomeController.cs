b4e120671897bd728de7369a147245ea1d2f66aa
gotoa1234 <cap8825@gmail.com>
2022-07-11 22:26:50 +0800

    0034. WebSite 後端判斷前端(用戶請求)是否透過ajax請求 範例代碼

---
+++ IsAjaxRequestExample/IsAjaxRequestExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace IsAjaxRequestExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AjaxCall()
+        {
+            ViewBag.IsAjax = Request.IsAjaxRequest();
+            var result = new
+            {
+                IsAjax = Request.IsAjaxRequest()
+            };
+            return Json(result);
+        }
+
+    }
+}
