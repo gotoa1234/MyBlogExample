badabd295278ab8391f870f67819ad9caa68b309
gotoa1234 <cap8825@gmail.com>
2023-07-19 17:10:23 +0800

    1. finish 圖形驗證碼登入 範例

---
+++ WebSiteCaptchaLoginExample/Common/Attriubte/CaptchaBindingAttribute.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using WebSiteCaptchaLoginExample.Common.Util;
+
+namespace WebSiteCaptchaLoginExample.Common.Attriubte
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class CaptchaBindingAttribute : Attribute, IActionFilter
+    {
+        /// <summary>
+        /// 圖形驗證碼綁定名稱
+        /// </summary>
+        public string CaptchaBindingName { get; set; } = string.Empty;
+
+        public bool Generate { get; set; } = false;
+
+        //1. 有綁定 [CaptchaBinding] 的Action才會觸發
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            try
+            {
+                //2. 配置當前"設定名稱"
+                var attribute = context.ActionDescriptor.EndpointMetadata.OfType<CaptchaBindingAttribute>().FirstOrDefault();
+                if (attribute != null)
+                {
+                    SessionUtil.CaptChaBindName = attribute.CaptchaBindingName;
+                    //3. 配置時為產生 Generate = true 才會生成字符串
+                    if (Generate)
+                    {
+                        SessionUtil.CaptCha = CaptchaUtil.GetRandomCaptcha(5);
+                    }
+                }
+            }
+            catch
+            {
+                //直接拋棄，不影響正式流程
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+    }
+}
--- WebSiteCaptchaLoginExample/Controllers/HomeController.cs
+++ WebSiteCaptchaLoginExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebSiteCaptchaLoginExample.Common.Attriubte;
 using WebSiteCaptchaLoginExample.Common.BaseController;
 using WebSiteCaptchaLoginExample.Common.Util;
 using WebSiteCaptchaLoginExample.Models;
@@ -21,12 +22,11 @@ namespace WebSiteCaptchaLoginExample.Controllers
 
         /// <summary>
         /// 用戶登入
-        /// </summary>
-        /// <returns></returns>
+        /// </summary>        
+        [CaptchaBinding(CaptchaBindingName = nameof(UserLogin), Generate = true)]
         public IActionResult UserLogin()
-        {
-            HttpContext.Session.SetString(nameof(SessionUtil.CaptCha), CaptchaUtil.GetRandomCaptcha(5));
-
+        {            
+            //4. 使用 CaptchaUtil.GetCapChatImg 產生Base64的驗證碼圖形
             var result = new LoginViewModel()
             {
                 Chapcha = $@"data:image/jpeg;base64,{Convert.ToBase64String(CaptchaUtil.GetCapChatImg(SessionUtil.CaptCha))}",
@@ -34,21 +34,27 @@ namespace WebSiteCaptchaLoginExample.Controllers
             return View(result);
         }
 
+        /// <summary>
+        /// 用戶登入按鈕-進行登入
+        /// </summary>        
         [HttpPost]
+        [CaptchaBinding(CaptchaBindingName = nameof(UserLogin))]
         public IActionResult UserLoginVerify(LoginViewModel inputData)
         {
-            return Ok("成功");
+            //1. 進行登入時會依照綁定的"設置名稱" 取得對應頁面的-圖形驗證碼，就不會造成A頁面卻吃到B頁面驗證碼的錯誤
+            if (inputData.SubmitData.InputCaptcha == SessionUtil.CaptCha)
+                return Ok("驗證成功");
+            else
+                return Ok("登入失敗");
         }
 
 
         /// <summary>
         /// 管理者登入
-        /// </summary>
-        /// <returns></returns>
+        /// </summary>        
+        [CaptchaBinding(CaptchaBindingName = nameof(AdminLogin), Generate = true)]        
         public IActionResult AdminLogin()
         {
-            HttpContext.Session.SetString(nameof(SessionUtil.CaptCha), CaptchaUtil.GetRandomCaptcha(5));
-
             var result = new LoginViewModel()
             {
                 Chapcha = $@"data:image/jpeg;base64,{Convert.ToBase64String(CaptchaUtil.GetCapChatImg(SessionUtil.CaptCha))}",
@@ -56,10 +62,17 @@ namespace WebSiteCaptchaLoginExample.Controllers
             return View(result);
         }
 
+        /// <summary>
+        /// 管理者登入按鈕-進行登入
+        /// </summary>
         [HttpPost]
+        [CaptchaBinding(CaptchaBindingName = nameof(AdminLogin))]
         public IActionResult AdminLoginVerify(LoginViewModel inputData)
         {
-            return Ok("成功");
+            if (inputData.SubmitData.InputCaptcha == SessionUtil.CaptCha)
+                return Ok("驗證成功");
+            else
+                return Ok("登入失敗");
         }
 
     }
