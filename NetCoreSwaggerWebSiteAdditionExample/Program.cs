b8cbdbe3e0ac80cd297bc4d10de9e09ab4688e1a
gotoa1234 <cap8825@gmail.com>
2023-08-02 18:20:34 +0800

    1. addition Example

---
+++ NetCoreSwaggerWebSiteAdditionExample/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//1. Nuget安裝Swagger
+//2-1. 增加以下代碼，啟用Swagger
+builder.Services.AddSwaggerGen(o =>
+{
+    o.SwaggerDoc("v1", new OpenApiInfo { Title = "產生Swagger文件", Version = "v1" });
+    // 2-2. 加入xml檔案到Swagger
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    o.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    //3. 只有Dev(Debug Mode)時才會啟用
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
