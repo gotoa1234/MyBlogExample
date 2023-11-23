218762404de403d68bfc957544f5fa739e1e93d9
gotoa1234 <cap8825@gmail.com>
2023-11-23 12:08:50 +0800

    1. 調整註解

--- NetCoreMvcWebSiteWithGrpcExample/Services/MyGrpcTesterService.cs
+++ NetCoreMvcWebSiteWithGrpcExample/Services/MyGrpcTesterService.cs
@@ -3,6 +3,7 @@ using NetCoreMvcWebSiteWithGrpcExample;
 
 namespace NetCoreMvcWebSiteWithGrpcExample.Services
 {
+    //1. 主類名稱 MyGrpcTesterService 其中MyGrpcTester與 mygrpctest.proto 配置的要相同
     public class MyGrpcTesterService : MyGrpcTester.MyGrpcTesterBase
     {
         private readonly ILogger<MyGrpcTesterService> _logger;
@@ -11,6 +12,8 @@ namespace NetCoreMvcWebSiteWithGrpcExample.Services
             _logger = logger;
         }
 
+        //2. 測試代碼，HTTP/2 接口名稱 SayHello ，如果有人帶 HelloRequest 的請求參數
+        //   我們返回 HelloReply 的返回內容
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
             return Task.FromResult(new HelloReply
