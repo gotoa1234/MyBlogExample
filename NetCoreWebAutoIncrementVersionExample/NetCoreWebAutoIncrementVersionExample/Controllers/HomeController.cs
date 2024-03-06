242d95bbc64d654a9c12805beed3875b9d733772
gotoa1234 <cap8825@gmail.com>
2024-03-06 16:56:44 +0800

    1. add draft example.

---
+++ NetCoreWebAutoIncrementVersionExample/NetCoreWebAutoIncrementVersionExample/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+namespace NetCoreWebAutoIncrementVersionExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Assembly assembly = Assembly.GetEntryAssembly();            
+            ViewBag.version = "Not Load";
+            if (assembly != null)
+            {
+                // 取得當前執行的程式集的 AssemblyInformationalVersionAttribute 屬性
+                var attribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+                if (attribute != null)
+                {                    
+                    ViewBag.version = attribute.InformationalVersion;                    
+                }
+            };
+            return View();
+        }
+    }
+}
