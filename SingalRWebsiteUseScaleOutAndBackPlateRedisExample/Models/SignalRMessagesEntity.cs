c1bc7aa83abafa7fb58ea24ed23f98c11578cbfc
gotoa1234 <cap8825@gmail.com>
2024-04-26 18:18:57 +0800

    1. fix draft

--- SingalRWebsiteUseScaleOutAndBackPlateRedisExample/Models/SignalRMessagesEntity.cs
+++ SingalRWebsiteUseScaleOutAndBackPlateRedisExample/Models/SignalRMessagesEntity.cs
@@ -2,16 +2,12 @@
 {
     public class SignalRMessagesEntity
     {
-		public int SignalRMessagesId { get; set; }
-
-		public string UserId { get; set; } = string.Empty;
+		public string UserName { get; set; } = string.Empty;
 
 		public string Message { get; set; } = string.Empty;
 
 		public int SiteValues { get; set; }
 
-		public DateTime CreateTime { get; set; }
-
-		public DateTime UpdateTime { get; set; }
+		public long CreateTime { get; set; }
 	}
 }
--- SingalRWebsiteUseScaleOutAndBackPlateRedisExample/SignalR/UpdateHub.cs
+++ SingalRWebsiteUseScaleOutAndBackPlateRedisExample/SignalR/UpdateHub.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using SingalRWebsiteUseScaleOutAndBackPlateRedisExample.Models;
 using StackExchange.Redis;
 
 namespace SingalRWebsiteUseScaleOutAndBackPlateRedisExample.SignalR
@@ -8,6 +10,7 @@ namespace SingalRWebsiteUseScaleOutAndBackPlateRedisExample.SignalR
         private readonly IConfiguration _configure;
         private readonly IDatabase _redisDatabase;
         private static string _Site = string.Empty;
+        private static string _RedisKey = "MyRadisSignalR";
         private int _siteNumber = 0;
         public UpdateHub(IDatabase redisDatabase)
         {
@@ -19,6 +22,22 @@ namespace SingalRWebsiteUseScaleOutAndBackPlateRedisExample.SignalR
 
             //1. 求出自己站點編號的 2 ^ (SiteNumber-1) 值 EX: 編號1=1 / 編號2=2 / 編號3=4
             _siteNumber = (int)Math.Pow(2, (_configure.GetValue("SiteNumber", 1) - 1));
+
+            SubScribeRedisEvent();
+        }
+
+        /// <summary>
+        /// SignalR Hub 訂閱 Redis ，讓每個訂閱 Redis 的 SignalR 可以同時推播
+        /// </summary>
+        private void SubScribeRedisEvent()
+        {            
+            var redisSubscriber = _redisDatabase.Multiplexer.GetSubscriber();
+            redisSubscriber.Subscribe("topic.test", async (channel, value) =>
+            {
+                // 當收到訂閱的事件通知時，發送更新訊息給所有的客戶端
+                //var updatedValue = await _redisDatabase.StringGetAsync(_RedisKey);
+                await Clients.All.SendAsync("ReceiveMessage", updatedValue);
+            });
         }
 
         /// <summary>
@@ -27,17 +46,17 @@ namespace SingalRWebsiteUseScaleOutAndBackPlateRedisExample.SignalR
         /// <returns></returns>
         public override async Task OnConnectedAsync()
         {
+            int startIndex = 0;
+            int endIndex = -1;
+
             // 從 Redis 中獲取聊天室的歷史訊息列表
-            var chatHistory = await _redisDatabase.ListRangeAsync("ChatHistory");
+            var chatHistory = await _redisDatabase.SortedSetRangeByRankAsync(_RedisKey, startIndex, endIndex);
 
             // 發送聊天室的歷史訊息給新連接的用戶
             foreach (var message in chatHistory)
             {
-                await Clients.Caller.SendAsync("ReceiveMessage", message);
+                await Clients.Caller.SendAsync("ReceiveMessage", message.ToString());
             }
-
-            //await Clients.Caller.SendAsync("ReceiveMessage", "");
-            //await base.OnConnectedAsync();
         }
 
         //事件名稱SendUpdate 行為:回傳message
@@ -51,11 +70,21 @@ namespace SingalRWebsiteUseScaleOutAndBackPlateRedisExample.SignalR
         /// </summary>                
         public async Task SendMessage(string user, string message)
         {
-            //3. 寫入資料庫 觸發SignalR 的 Database Backplane
-            await _redisDatabase.ListLeftPushAsync("ChatHistory", $"{user}: {message}");
+            var dataEntity = new SignalRMessagesEntity() {
+                Message = message,
+                SiteValues = _siteNumber,
+                UserName = user,
+                CreateTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+            };
 
-            //4. 回報前端，後端 Server 有收到訊息了
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            var jsonData= JsonConvert.SerializeObject(dataEntity);
+
+            //3. 寫入資料庫 觸發SignalR 的 Database Backplane
+            await _redisDatabase.SortedSetAddAsync(_RedisKey, jsonData, dataEntity.CreateTime);
+            
+            _redisDatabase.Publish("topic.test", "123");            
+            ////4. 回報前端，後端 Server 有收到訊息了
+            //await Clients.All.SendAsync("ReceiveMessage", jsonData);
         }
 
     }
