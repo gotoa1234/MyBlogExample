b5bccf7a59d55a4d61cb7bc8834927d85015d002
gotoa1234 <cap8825@gmail.com>
2024-08-22 18:35:44 +0800

    1. not yet finish code.

---
+++ Example.Common/RabbitMQ/Model/BaseModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Example.Common.RabbitMQ.Model
+{
+    public class BaseModel
+    {
+        /// <summary>
+        /// 主機位置
+        /// </summary>
+        public string HostName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 帳戶
+        /// </summary>
+        public string UserName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 密碼
+        /// </summary>
+        public string Password { get; set; } = string.Empty;        
+    }
+}
---
+++ Example.Common/RabbitMQ/RabbitMqMessageReceiver.cs
@@ -0,0 +1,71 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+
+namespace Example.Common.RabbitMQ
+{
+    public class RabbitMqMessageReceiver<TMessage> : IDisposable
+    {
+        private ConnectionFactory _factory;
+        private bool _disposed;
+        private IModel _channel;
+        private IConnection _connection;
+        private EventingBasicConsumer _consumer;
+
+        public RabbitMqMessageReceiver()
+        {
+            _concurrentCount = concurrentCount;
+            _receivedAction = action;
+            _autoAck = autoAck;
+
+            _factory = new ConnectionFactory
+            {
+                HostName = rabbitParameters.HostName,
+                UserName = rabbitParameters.UserName,
+                Password = rabbitParameters.Password,
+
+                RequestedHeartbeat = TimeSpan.FromSeconds(30),
+                AutomaticRecoveryEnabled = true, // 自動重連
+                TopologyRecoveryEnabled = true, // 
+                NetworkRecoveryInterval = TimeSpan.FromSeconds(10)
+            };
+
+            //var type = rabbitParameters.ExchangeType.ToString().ToLower();
+            //if (rabbitParameters.ExchangeType != ExchangeTypeEnum.Fanout)
+            //{
+            //    rabbitParameters.ExchangeName += $"_{type}";
+            //}
+
+            _exchangeType = rabbitParameters.ExchangeType;
+            _exchangeName = rabbitParameters.ExchangeName;
+
+            Connect();
+        }
+
+        ~RabbitMqMessageReceiver()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+            if (disposing)
+            {
+                _factory = null!;
+            }
+            _channel?.Dispose();
+            _connection?.Dispose();
+            _disposed = true;
+        }
+    }
+}
