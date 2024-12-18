d5074218c15fe8d167d64d61f29a0ec5fb9e6fee
gotoa1234 <cap8825@gmail.com>
2024-12-18 17:57:55 +0800

    1. finish example.

--- RedisProducerAndComsumerForListExample/Controllers/HomeController.cs
+++ RedisProducerAndComsumerForListExample/Controllers/HomeController.cs
@@ -1,19 +1,44 @@
 using Microsoft.AspNetCore.Mvc;
+using RedisProducerAndComsumerForListExample.Service;
 
 namespace RedisProducerAndComsumerForListExample.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly RedisQueueService _queueService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger
+            , RedisQueueService queueService)
         {
+            _queueService = queueService;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// 2. 實現 List Redis 生產者推送訊息
+        /// </summary>
+        [HttpPost]
+        public IActionResult ProduceMessage([FromBody] MessageRequest request)
+        {
+            if (string.IsNullOrEmpty(request.Message))
+            {
+                return BadRequest("Message is required.");
+            }
+            request.Message = request.Message + $@" => 傳送資料的時間 : {DateTime.Now}";
+
+            _queueService.Enqueue("myQueue", request.Message);
+            return Ok(new { message = "Message added to queue: " + request.Message });
+        }
+
+        public class MessageRequest
+        {
+            public string Message { get; set; }
         }
     }
 }
--- RedisProducerAndComsumerForPubAndSubExample/Service/RedisPubSubService.cs
+++ RedisProducerAndComsumerForPubAndSubExample/Service/RedisPubSubService.cs
@@ -13,11 +13,17 @@ namespace RedisProducerAndComsumerForPubAndSubExample.Service
             _subscriber = _redis.GetSubscriber();
         }
 
+        /// <summary>
+        /// 1. 提供立即推送到 Redis 上 Subscribe 的對象
+        /// </summary>
         public void Publish(string channel, string message)
         {
             _subscriber.Publish(channel, message);
         }
 
+        /// <summary>
+        /// 2. 提供對象進行訂閱，在這篇範例是背景服務
+        /// </summary>
         public void Subscribe(string channel, Action<string> messageHandler)
         {
             _subscriber.Subscribe(channel, (redisChannel, value) =>
