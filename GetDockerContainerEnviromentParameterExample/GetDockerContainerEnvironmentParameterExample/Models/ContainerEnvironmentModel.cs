60c04622f4e2770f5275c716024442a1e763b6d5
Louis <louis@evervictory-it.com>
2025-06-10 11:55:04 +0800

    1. Add example

---
+++ GetDockerContainerEnviromentParameterExample/GetDockerContainerEnvironmentParameterExample/Models/ContainerEnvironmentModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GetDockerContainerEnvironmentParameterExample.Models
+{
+    public class ContainerEnvironmentModel
+    {
+        public string AspNetCoreEnvironment =>
+                    Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? string.Empty;
+
+        public string DotNetEnvironment =>
+            Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? string.Empty;
+
+        public string SecurityKey =>
+            Environment.GetEnvironmentVariable("security_key") ?? string.Empty;
+    }
+}
