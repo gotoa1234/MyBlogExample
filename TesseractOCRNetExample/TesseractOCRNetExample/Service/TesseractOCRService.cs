d52e237f02d1fcf37ddd430e3080431e82a5e3e3
gotoa1234 <cap8825@gmail.com>
2025-08-28 01:19:08 +0800

    1. complete example.

--- TesseractOCRNetExample/TesseractOCRNetExample/Service/TesseractOCRService.cs
+++ TesseractOCRNetExample/TesseractOCRNetExample/Service/TesseractOCRService.cs
@@ -18,20 +18,21 @@ namespace TesseractOCRNetExample.Service
         /// <returns></returns>
         public string TesseractOCRVersionImage()
         {
-            // 語言包路徑
+            // 1. 語言包路徑
             string tessDataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $@"Source\tessdata");
-            
-            // 解析的文字內容
+
+            // 2. 解析的文字內容
             var result = string.Empty;
             try
             {
-                // 使用的語言
+                // 3. 設定語言包，必須使用副檔名前的名稱，並且用 List 才能支持多語言
                 var languages = new List<Language>() { Language.English, Language.ChineseSimplified };
-                
                 using (var engine = new Engine(tessDataPath, languages, EngineMode.Default))
                 {
+                    // 4. 讀取圖片
                     using (var img = TesseractOCR.Pix.Image.LoadFromFile(StaticHelper._ImageFileName))
                     {
+                        // 5. 執行 OCR 結果
                         using (var page = engine.Process(img))
                         {                            
                             result = page.Text;
--- TesseractOCRNetExample/TesseractOCRNetExample/Service/TesseractService.cs
+++ TesseractOCRNetExample/TesseractOCRNetExample/Service/TesseractService.cs
@@ -17,17 +17,20 @@ namespace TesseractOCRNetExample.Service
         /// <returns></returns>
         public string TesseractVersionImage()
         {
-            // 語言包路徑
+            // 1. 語言包路徑
             var tessDataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $@"Source\tessdata");
 
-            // 解析的文字內容
+            // 2. 解析的文字內容
             var result = string.Empty;
             try
             {
-                using (var engine = new TesseractEngine(tessDataPath, "chi_sim", EngineMode.Default))
+                // 3. 設定語言包，必須使用副檔名前的名稱，並且用 + 才能支持多語言
+                using (var engine = new TesseractEngine(tessDataPath, "eng+chi_sim", EngineMode.Default))
                 {
+                    // 4. 讀取圖片
                     using (var img = Pix.LoadFromFile(StaticHelper._ImageFileName))
                     {
+                        // 5. 執行 OCR 結果
                         using (var page = engine.Process(img))
                         {                            
                             result = page.GetText();
